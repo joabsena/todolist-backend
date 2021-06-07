11eef977db922efd0a7ca2c73c2a0cb5abf133c3
Joab Sena <joabarbosa1@hotmail.com>
2021-06-07 20:18:58 -0300

    feat: create routes of users

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const router = Router();
 
-router.get('/todo', (req, res) => {
-  res.json({ message: 'ok' });
-});
+router.post('/users', UserController.store);
+
+export default router;
